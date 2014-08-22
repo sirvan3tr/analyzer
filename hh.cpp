301fa2e313c71d67a0b6ab01e61d43874ef7205d
Sirvan Almasi <sirvanalmasi@Sirvans-MacBook-Pro.local>
2014-08-22 03:04:23 +0100

    fool

--- hh.cpp
+++ hh.cpp
@@ -5,6 +5,7 @@
 
 #include <string>
 #include <sstream>
+#include <algorithm>
 
 
 using namespace  std;
@@ -12,20 +13,53 @@ using namespace  std;
 int main() {
     string line;
 
-    ifstream logfile ("glassesonly.csv");
-    ofstream outputfile ("excel/mylog/g.txt", ios::app);
-    int secs, mins, hours, t, timediff, dayscount;
+    ifstream logfile("glassesonly.csv");
+    //ifstream logfile("mylog/glassesonly.csv");
+    ofstream outputfile ("btest.txt", ios::app);
+    int lineone, newday, secs, mins, hours, t, timediff, dayscount, linecount, prevdaycount;
     char extra;
+    string  prevday = "";
+    string currentday = "";
+    string previp = "";
+    string currentip = "";
+    string delimiter = ",";
+    linecount = 0;
+    prevdaycount = 0;
+    dayscount = 1;
+    int sameip = 0;
+    int uniqueipcount = 0;
+
+
+
+    int totallines = count(istreambuf_iterator<char>(logfile), 
+             istreambuf_iterator<char>(), '\n') + 1;
+    //cout << totallines << endl;
+    logfile.close();
+    logfile.clear();
+    logfile.open("glassesonly.csv");
+    //logfile.open("mylog/glassesonly.csv");
     if (logfile.is_open()) {
         while ( getline (logfile,line) ) {
-            string delimiter = ",";
+  
             size_t pos = 0;
             string token;
-            string prevday;
-            size_t count = 0;
+            int count = 0;
                 while ((pos = line.find(delimiter)) != string::npos) {
-                    token = line.substr(0, pos);
-                        if (count==1) {
+                        token = line.substr(0, pos);
+                        if (count==0) {
+
+                            if(token==prevday) {
+                                prevdaycount = dayscount;
+                                dayscount = dayscount +1;
+                                newday = 0;
+                            } else {
+                                currentday = token;
+                                prevdaycount = dayscount;
+                                dayscount = 1;
+                                newday = 1;
+                            }
+
+                        } else if (count==1) {
                             // token  = current time in this if loop
                             stringstream ss;
                             ss << token;
@@ -34,26 +68,65 @@ int main() {
                                >> secs;
                             t = (hours*3600)+(mins*60)+secs;
 
-                            cout << t << endl;
-                        } else if (count==0) {
-                            if(token==prevday) {
-                                dayscount = dayscount +1;
+                            //cout << t << endl;
+                        } else if (count==4) {
+                            // manage the ip addresses here
+                            if (token==currentip) {
+                                sameip = 1;
                             } else {
-                                dayscount = 0;
+                                sameip = 0;
+                                currentip = token;
                             }
-                            cout << token << endl;
-                            token = prevday;
                         }
-                        //outputfile << token << " ";
+                        
                     line.erase(0, pos + delimiter.length());
                     count = count + 1;
 
                 }
                 //outputfile << line << endl;
-                cout << "NEWLINE" << endl;
-                count = 0;
+                if (newday==1) {
+                    sameip = 0;
+                }
+                if (sameip==0) {
+                    uniqueipcount = uniqueipcount + 1;
+                }
+
+                linecount = linecount+1;
+           
+                cout << "-----------" << endl;
+                cout << "same ip? " << sameip << endl;
+                cout << "Unique count: " << uniqueipcount << endl;
+                cout << "Current day: " << currentday << endl;
+                cout << "Time: " << t << endl;
+                cout << totallines << endl;
+                cout << linecount << " NEWLINE" << endl;
+                
+                cout << "Prev day: " << prevday << endl;
                 cout << dayscount << endl;
+                cout << "prev day count: " << prevdaycount << endl;
+                cout << "new day: " << newday << endl;
+                /**/
+                //cout << "the lines " << totallines << " " << linecount << endl; 
+                count = 0;
+                if ((newday == 1) && linecount>1) {
+                    /*if (linecount==totallines) {
+                        outputfile << prevday << "," << prevdaycount << endl;
+                        outputfile << currentday << "," << dayscount  << endl;                    
+                    } else {*/
+                        outputfile << prevday << "," << prevdaycount << "," << uniqueipcount << endl; 
+                    //}
+                } else if (linecount==totallines) {
+                    outputfile << currentday << "," << dayscount << "," << uniqueipcount << endl;
+                }
+                if (newday==1) {
+                    sameip = 0;
+                    uniqueipcount = 1;
+                }
+                prevday = currentday;
+                previp = currentip;
+                
         }
+        cout << "Total Downloads: " << linecount << endl;
         logfile.close();
     }
     else cout << "Unable to open file";
